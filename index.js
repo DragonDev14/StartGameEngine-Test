580b045b7917db4169f3cdf8d8c015c1d7388527
cs3583564 <palovi3956@boldhut.com>
2021-03-08 16:34:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var x = true;
+var y = false;
+var test = 710
+var start = "startrun";
+
+
+
+
+
+
+
+if (x == true)
+{
+  y = true;
+  x = false;
+  test = test + 200;
+  start = "startrun" + 10
+
+  if(y == true)
+  {
+    test = test - 230
+    test = test - 10
+  }
+
+  if(start = "startrun" + 10)
+  {
+    test = test - 200
+  }
+  
+}
